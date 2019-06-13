19c12e930b6063fabb7e5786e270a30c91617581
phileasFoggy22 <phileas.foggy@googlemail.com>
2019-06-13 17:33:38 +0100

    DB Repos Part made

--- src/main/java/com/bae/persistence/repository/UserDBRepository.java
+++ src/main/java/com/bae/persistence/repository/UserDBRepository.java
@@ -7,11 +7,14 @@ import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
 
-import com.bae.persistence.domain.Activity;
+import com.bae.persistence.domain.User;
 import com.bae.util.JSONUtil;
+
+
 
 @Transactional(SUPPORTS)
 @Default
@@ -25,34 +28,43 @@ public class UserDBRepository implements UserRepository {
 	@Override
 	@Transactional(TxType.REQUIRED)
 	public String createUser(String userJSON) {
-		// TODO Auto-generated method stub
-		return null;
+		User newUser = util.getObjectForJSON(userJSON, User.class);
+		System.out.println("error: " + newUser);
+		manager.persist(newUser);
+		return "{\"message\": \"user successfully added\"}";
 	}
 
 	@Override
 	public String getUser(String userEmail) {
-		// TODO Auto-generated method stub
-		return null;
+		User aUser = manager.find(User.class, userEmail);
+		return util.getJSONForObject(aUser);
 	}
 
 	@Override
 	public String getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<User> query = manager.createQuery("SELECT a FROM Users a", User.class);
+		return util.getJSONForObject(query.getResultList());
 	}
 
 	@Override
 	@Transactional(TxType.REQUIRED)
 	public String updateUser(String userJSON, String userEmail) {
-		// TODO Auto-generated method stub
-		return null;
+		User theUser = util.getObjectForJSON(userJSON, User.class);
+		User updateThisUser = manager.find(User.class, userEmail);
+		updateThisUser.setUserEmail(theUser.getUserEmail());
+		updateThisUser.setUserName(theUser.getUserName());
+		updateThisUser.setUserPassword(theUser.getUserPassword());
+		manager.persist(updateThisUser);
+		return "{\"message\": \"user successfully updated\"}";
+
 	}
 
 	@Override
 	@Transactional(TxType.REQUIRED)
 	public String deleteUser(String userEmail) {
-		// TODO Auto-generated method stub
-		return null;
+		User aUser = manager.find(User.class, userEmail);
+		manager.remove(aUser);
+		return "{\"message\": \"User sucessfully removed\"}";
 	}
 	
 }
