222afa4af7739c63adae6ee4cc8533774979fa68
phileasFoggy22 <phileas.foggy@googlemail.com>
2019-06-18 16:55:12 +0100

    some changes

--- src/test/java/com/bae/repository/test/Version2IterationTest.java
+++ src/test/java/com/bae/repository/test/Version2IterationTest.java
@@ -1,5 +1,7 @@
 package com.bae.repository.test;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
@@ -10,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.bae.persistence.domain.User;
 import com.bae.persistence.repository.UserDBRepository;
 import com.bae.util.JSONUtil;
 
@@ -28,28 +31,30 @@ public class Version2IterationTest {
 
 	private static final String MOCK_DATA_ARRAY = "[{\"userEmail\":\"L.Cravensworth@gmail.com\",\"userName\":\"Laszlo Cravensworth\",\"password\":\"password\"}]";
 	private static final String MOCK_OBJECT = "{\"userEmail\":\"L.Cravensworth@gmail.com\",\"userName\":\"Laszlo Cravensworth\",\"password\":\"passord\"}";
+	private static final String MOCK_OBJECT2 = "{\"userEmail\":\"N.Cravensworth@gmail.com\",\"userName\":\"Nadja Cravensworth\",\"password\":\"passord\"}";
+	private static final String MOCK_OBJECT3 = "{\"userEmail\":\"N.Relentless@gmail.com\",\"userName\":\"Nandor the Relentless\",\"password\":\"passord\"}";
 
 	@Before
 	public void start() {
 		repo.setManager(manager);
 		util = new JSONUtil();
 		repo.setUtil(util);
+//		repo.createUser(MOCK_OBJECT2);
 	}
 
 	@Test
 	public void addUser() {
-		System.out.println(MOCK_OBJECT);
-		// System.out.println(MOCK_DATA_ARRAY);
-		// System.out.println("here "+util.getJSONForObject(MOCK_OBJECT));
-		// System.out.println("and
-		// "+repo.createUser(util.getJSONForObject(MOCK_OBJECT)));
-		// System.out.println(usersDB.getUser("L.Cravensworth@gmail.com"));
-		// assertEquals(newUser,usersDB.getUser("L.Cravensworth@gmail.com"));
-		assert (true);
+//		 String reply = repo.createUser(MOCK_OBJECT);
+		String reply = repo.createUser(MOCK_OBJECT);
+		assertEquals("{\"message\": \"user successfully added\"}",reply);
 	}
 
 	@Test
 	public void updateUser() {
+		User toUpdate  = util.getObjectForJSON(repo.getUser("N.Cravensworth@gmail.com"), User.class);
+		User newValues = util.getObjectForJSON(MOCK_OBJECT3, User.class);
+//		String reply = repo.updateUser(MOCK_OBJECT3, "N.Cravensworth@gmail.com");
+//		assertEquals("{\"message\": \"user successfully added\"}",reply);
 		assert (true);
 	}
 
